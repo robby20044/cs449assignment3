16321fcaa16401c743f52994a2647a5b660aa33f
Robby Nielson <160625956+robby20044@users.noreply.github.com>
2026-03-25 20:39:46 +0000

    Add files via upload

---
+++ CS_449_Assignment_3/addFriend.js
@@ -0,0 +1,11 @@
+<script type="text/javascript">
+window.onload = function () {
+    var Ajax=null;
+    // XXXX is to be filled with a URL that 
+    // triggers the request to add Samy as a friend
+    var sendurl=XXXX
+    Ajax=new XMLHttpRequest();
+    Ajax.open("GET", sendurl, true);
+    Ajax.send();
+}
+</script>
